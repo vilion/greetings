fab4a93626aa73718c6e14f3c3ba314862135685
vilion <none@none.com>
2015-09-20 18:59:24 +0900

    add test

---
+++ src/test/java/org/greetings/service/GreetingsTest.java
@@ -0,0 +1,21 @@
+package org.greetings.service;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import org.greetings.service.Greetings;
+import org.junit.Test;
+
+
+public class GreetingsTest{
+
+    @Test
+    public void shouldCalculateTotalPrice() {
+        Greetings greetings = new Greetings();
+
+        greetings.greetingsGenerator = new RandomGreetingsGenerator();
+
+        String message = greetings.generatesGreeting(1);
+        assertThat(message, containsString("John Smith"));
+    }
+}
